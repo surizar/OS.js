3b6d5705906a023ee438ae234b4a4dd36e37126c
Anders Evenrud <andersevenrud@gmail.com>
2016-11-21 21:21:33 +0100

    Utils: Preloader now checks if absolute/relative path properly after changes

--- src/client/javascript/utils/xhr.js
+++ src/client/javascript/utils/xhr.js
@@ -413,7 +413,10 @@
           item = {src: item};
         }
 
-        var src = OSjs.API.getBrowserPath(item.src);
+        var src = item.src;
+        if ( !src.match(/^(\/|https?)/) ) {
+          src = OSjs.API.getBrowserPath(item.src);
+        }
         item._src = src;
         item.src = src;
         item.type = item.type ? getTypeCorrected(item.type) : getType(item.src);
